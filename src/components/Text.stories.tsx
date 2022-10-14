bec4d1337a75d510729e3f42a8af3d9e93243ea6
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2022-10-14 17:56:01 -0300

    feat: add storybook structure, design tokens, and text component

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,53 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Text, TextProps } from './Text';
+
+export default {
+  title: 'Componets/Text',
+  component: Text,
+  args: {
+    size: 'md',
+    children: 'Text'
+  },
+  argTypes: {
+    size: {
+      options: ['xs', 'sm', 'md', 'lg'],
+      control: {
+        type: 'inline-radio'
+      }
+    }
+  }
+} as Meta<TextProps>;
+
+export const Default: StoryObj<TextProps> = {};
+
+export const ExtraSmall: StoryObj<TextProps> = {
+  args: {
+    size: 'xs'
+  }
+};
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+};
+
+export const Large: StoryObj<TextProps> = {
+  args: {
+    size: 'lg'
+  }
+};
+
+export const CustomComponent: StoryObj<TextProps> = {
+  args: {
+    asChild: true,
+    children: <p>Testando</p>
+  },
+  argTypes: {
+    children: {
+      table: {
+        disable: true
+      }
+    }
+  }
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.tsx'],
+  theme: {
+    fontSize: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 20,
+      '2xl': 24,
+      '3xl': 32
+    },
+    colors: {
+      'chestnut-600': '#B7524E',
+      'cyan-700': '#5098C9',
+      'cyan-600': '#69AEDE',
+      'gray-900': '#121214',
+      'gray-800': '#202024',
+      'gray-400': '#7C7C8A',
+      'gray-200': '#E1E1E6',
+      'gray-100': '#C4C4CC'
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    }
+  },
+  plugins: []
+};
