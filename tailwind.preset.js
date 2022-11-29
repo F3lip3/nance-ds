6a382046d1a88d1b5de66d244e9797fa44d24891
Felipe Humberto Teixeira <felipe@dinfo.com.br>
2022-11-29 01:07:30 -0300

    feat: add tailwind preset

---
+++ tailwind.preset.js
@@ -0,0 +1,38 @@
+module.exports = {
+  theme: {
+    fontSize: {
+      xs: 12,
+      sm: 14,
+      md: 16,
+      lg: 18,
+      xl: 20,
+      '2xl': 24,
+      '3xl': 32
+    },
+    colors: {
+      black: '#000',
+      chestnut: {
+        600: '#B7524E'
+      },
+      cyan: {
+        600: '#69AEDE',
+        700: '#5098C9'
+      },
+      gray: {
+        100: '#C4C4CC',
+        200: '#E1E1E6',
+        400: '#7C7C8A',
+        800: '#202024',
+        900: '#121214'
+      },
+      transparent: 'transparent',
+      white: '#fff'
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif'
+      }
+    }
+  },
+  plugins: []
+};
